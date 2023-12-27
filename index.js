07a67fd5674f8ea0138ea459e35b2d3593612f2f
Nikita Batryncha <batrynchan@gmail.com>
2023-12-27 04:39:54 +0300

    v3

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-import AppExtensionsSDK from '@pipedrive/app-extensions-sdk';
-
-// SDK detects identifier from URL and uses default
-      sizeconstsdk = await new AppExtensionsSDK().initialize();
-
-// Pass in id manually and provide custom UI size
-const sdk = await new AppExtensionsSDK({ identifier: '123abc' })
-  .initialize({ size: { height: 500 } });
+document.getElementById('xyz').addEventListener('submit', async (event) => {
+  event.preventDefault();
+  console.log('Form submitted successfully!');
+});
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+    fallback: {
+      querystring: require.resolve('querystring-es3'),
+    },
+  },
+};
