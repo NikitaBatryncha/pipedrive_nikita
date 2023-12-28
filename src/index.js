37ae8a7de6bc9e67e0f53ff94a0a098778be5c8f
Nikita Batryncha <batrynchan@gmail.com>
2023-12-28 19:40:44 +0300

    v12

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const scheduled = FORM.querySelector('.scheduled');
 
 //Here I initialize input types
 const text = 'text';
+const number = 'number';
 const tel = 'tel';
 const email = 'email';
 const date = 'date';
@@ -23,14 +24,107 @@ function createInput(type, id, placeholder) {
   return input;
 }
 
-// Put inputs in form
+function createSelect(id, value) {
+  const select = document.createElement('select');
+  selectPlaceholder = document.createElement('option');
+  selectPlaceholder.text = value;
+  select.id = id;
+  select.append(selectPlaceholder)
+  return select
+}
 
+function createOpt(value) {
+  const option = document.createElement('option');
+  option.value = value;
+  option.text = value;
+  option.style.fontSize = '12px';
+  option.style.fontWeight = '500';
+  option.style.color = '#868686';
+  return option
+}
+
+// Put inputs in form
 const nameInpt = createInput(text, 'name', 'First name');
 const lastnameInpt = createInput(text, 'surname', 'Last name');
 const telInpt = createInput(tel, 'tel', 'Phone');
 const emailInpt = createInput(email, 'email', 'Email (optional)');
-client_details.append(nameInpt, lastnameInpt, telInpt, emailInpt)
- FORM.addEventListener('submit', async (event) => {
+const client__wrapper = client_details.querySelector('.input__wrapper--1');
+client__wrapper.append(nameInpt, lastnameInpt)
+client_details.append(client__wrapper, telInpt, emailInpt);
+
+const jobtypeSelector = createSelect('Job_type', 'Job type');
+const jobsourceSelector = createSelect('Job_source', 'Job source');
+const jobtypeWrapper = job_type.querySelector('.input__wrapper--2');
+jobtypeWrapper.append(jobtypeSelector, jobsourceSelector);
+const textarea = document.createElement('textarea');
+textarea.placeholder = 'Job description (optional)'
+job_type.append(jobtypeWrapper, textarea);
+
+const addressInpt = createInput(text, 'address', 'Adress');
+const cityInpt = createInput(text, 'city', 'City');
+const stateInpt = createInput(text, 'state', 'State');
+const zipInpt = createInput(number, 'zip', 'Zip code');
+const areaSelect = createSelect('area', 'Area');
+
+const serviceWrapper = service_location.querySelector('.input__wrapper--3')
+serviceWrapper.append(zipInpt, areaSelect);
+service_location.append(addressInpt, cityInpt, stateInpt, serviceWrapper)
+
+const startdateInpt = createInput(date, 'start_date', 'Start date');
+const starttimeInpt = createInput(time, 'start_time', 'Start time');
+const endtimeInpt = createInput(time, 'end_time', 'End time');
+const scheduledWrapper = scheduled.querySelector('.input__wrapper--4');
+scheduledWrapper.append(starttimeInpt, endtimeInpt);
+const testSelect = createSelect('test', 'Test select');
+scheduled.append(startdateInpt, scheduledWrapper, testSelect);
+
+const createDealInPipedrive = async (dealData) => {
+  const apiUrl = 'https://zany-replace.pipedrive.com/v1/deals?api_token=9299a8a8cee523f120b7c2f26c807983d7ce7686';
+
+  try {
+    const response = await axios.post(apiUrl, dealData, {
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer 9299a8a8cee523f120b7c2f26c807983d7ce7686'
+      },
+    });
+
+
+    console.log('Deal created successfully:', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error creating deal:', error);
+    throw error;
+  }
+};
+
+const dealData = {
+  title: nameInpt.value,
+  value: 123,
+  currency: 'USD',
+  user_id: 12960094,
+  org_id: 1,
+};
+
+FORM.addEventListener('submit', async (event) => {
   event.preventDefault();
-  console.log(nameInpt.value, lastnameInpt.value, telInpt.value, emailInpt.value);
+  createDealInPipedrive(dealData)
+  // console.log(
+  //   nameInpt.value,
+  //   lastnameInpt.value,
+  //   telInpt.value,
+  //   emailInpt.value,
+  //   jobtypeSelector.value,
+  //   jobsourceSelector.value,
+  //   textarea.value,
+  //   addressInpt.value,
+  //   cityInpt.value,
+  //   stateInpt.value,
+  //   zipInpt.value,
+  //   areaSelect.value,
+  //   startdateInpt.value,
+  //   starttimeInpt.value,
+  //   endtimeInpt.value,
+  //   testSelect.value,
+  //   );
 });
