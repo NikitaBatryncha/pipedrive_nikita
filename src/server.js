8822b2cff178dd4488d8d406bead6cbe61a32e84
Nikita Batryncha <batrynchan@gmail.com>
2023-12-29 01:59:17 +0300

    56

--- src/server.js
+++ src/server.js
@@ -1,21 +1,15 @@
 const successMessage = document.createElement('label');
 successMessage.style.color = 'green';
 
-const dealIdFromUrl = window.location.href.split('/').pop();
-console.log('Deal ID from URL:', dealIdFromUrl);
-
 const createDealInPipedrive = async (dealData) => {
   // const apiUrl = 'https://zany-replace.pipedrive.com/v1/deals?api_token=9299a8a8cee523f120b7c2f26c807983d7ce7686';
-  // const apiUrl = `https://zany-replace.pipedrive.com/v1/deals/${dealIdFromUrl}`;
-  const apiUrl = `https://zany-replace.pipedrive.com/deal/84`;
-
+  const apiUrl = 'https://zany-replace.pipedrive.com/v1/deals/84?api_token=9299a8a8cee523f120b7c2f26c807983d7ce7686';
   try {
     const response = await axios.put(apiUrl, dealData, {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Authorization': 'Bearer 9299a8a8cee523f120b7c2f26c807983d7ce7686'
-      },
+       },
     });
     console.log('Deal updated successfully:', response.data);
     if (response.data.success === true) {
