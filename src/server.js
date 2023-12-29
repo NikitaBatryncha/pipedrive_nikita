a3f5c42d824492b003b4c6e4340ae991c83432f9
Nikita Batryncha <batrynchan@gmail.com>
2023-12-29 03:38:56 +0300

    v62

--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ successMessage.style.color = 'green';
 
 const dealIdFromUrl = window.location.href.split('selectedIds=').pop();
 const match = dealIdFromUrl.match(/selectedIds=([^&]*)/);
-const selectedIds = match && match[1];
-console.log(selectedIds);
+console.log(match);
 console.log('Deal ID from URL:', dealIdFromUrl);
 
 const createDealInPipedrive = async (dealData) => {;
